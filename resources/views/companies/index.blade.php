02fe1bef507c9a85713032efd3e18faa46778253
Philip <philipgolo2323@gmail.com>
2023-05-17 01:02:24 +0800

    i change the background color

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <title>Laravel 9 CRUD Tutorial Example</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >    
-<style>
-    body{
-        background-color: #8d06fd;
-    }
+    <style>
+        body{
+            background-color: rgb(202, 221, 237);
+        }
 
-</style>
+
+    </style>
 </head>
 <body>
     <div class="container mt-2">
